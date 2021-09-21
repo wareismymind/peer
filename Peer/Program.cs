172dc4f4070c24a8bcf99c7153bd7f5183d3a32c
Chris Nantau <Chris.Nantau@Gmail.com>
2021-09-21 12:09:40 -0300

    feat: initial console application

---
+++ Peer/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Peer
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
