f4448b80477755f15056ffa870e614929b03b7cc
Chris Nantau <Chris.Nantau@Gmail.com>
2021-10-10 15:19:02 -0300

    initial impl of show and parsing. Stubbed IPeerApplication interface and Open options

--- Peer/Program.cs
+++ Peer/Program.cs
@@ -1,95 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Peer.Domain;
-using Peer.Domain.Formatters;
+using CommandLine;
+using Peer.Verbs;
 
 namespace Peer
 {
     public static class Program
     {
-        private static readonly Random _rando = new();
-
-        public static async Task Main(string[] _)
+        public static async Task Main(string[] args)
         {
-            var consoleJiggy = new ConsoleWriter();
+            var res = await Parser.Default.ParseArguments<ShowOptions, OpenOptions>(args)
+                .MapResult(
+                    (ShowOptions opts) => Task.FromResult("Whee"),
+                    err => Task.FromResult("Saaad"));
 
-            var lines = new[]
-            {
-                "doot",
-                "waka",
-                "fsd;lkfjsd;lfkajsd;lkfjasdf",
-                "sdfka;sldkfjas;ldkfja;sdlkfjas;dlfkjasd;flk",
-                "asjsdlfkjds",
-                "dooooot",
-                "wakaaaa",
-                "ASDJKHASLKJDHASLKJDHALKSJD",
-                "doot",
-                "waka",
-                "fsd;lkfjsd;lfkajsd;lkfjasdf",
-                "sdfka;sldkfjas;ldkfja;sdlkfjas;dlfkjasd;flk",
-                "asjsdlfkjds",
-                "dooooot",
-                "wakaaaa",
-                "ASDJKHASLKJDHASLKJDHALKSJD",
-                "doot",
-                "waka",
-                "fsd;lkfjsd;lfkajsd;lkfjasdf",
-                "sdfka;sldkfjas;ldkfja;sdlkfjas;dlfkjasd;flk",
-                "asjsdlfkjds",
-                "dooooot",
-                "wakaaaa",
-                "ASDJKHASLKJDHASLKJDHALKSJD",
-                "doot",
-                "waka",
-                "fsd;lkfjsd;lfkajsd;lkfjasdf",
-                "sdfka;sldkfjas;ldkfja;sdlkfjas;dlfkjasd;flk",
-                "asjsdlfkjds",
-                "dooooot",
-                "wakaaaa",
-                "ASDJKHASLKJDHASLKJDHALKSJD",
-                "doot",
-                "waka",
-                "fsd;lkfjsd;lfkajsd;lkfjasdf",
-                "sdfka;sldkfjas;ldkfja;sdlkfjas;dlfkjasd;flk",
-                "asjsdlfkjds",
-                "dooooot",
-                "wakaaaa",
-                "ASDJKHASLKJDHASLKJDHALKSJD",
-                "doot",
-                "waka",
-                "fsd;lkfjsd;lfkajsd;lkfjasdf",
-                "sdfka;sldkfjas;ldkfja;sdlkfjas;dlfkjasd;flk",
-                "asjsdlfkjds",
-                "dooooot",
-                "wakaaaa",
-                "ASDJKHASLKJDHASLKJDHALKSJD",
-                "LAST_LINE"
-            };
-
-            //Console.Clear();
-            //for (var i = 0; i < 100; i++)
-            //{
-            //    consoleJiggy.Display(lines.TakeLast(1 + _rando.Next(lines.Length)).OrderByDescending(x => x == "LAST_LINE" ? int.MinValue : _rando.Next(0, 200)).ToList(), default);
-            //    await Task.Delay(30);
-            //}
-            Console.OutputEncoding = Encoding.UTF8;
-            var prList = Enumerable.Range(0, Enum.GetValues<PullRequestStatus>().Length).Select(x =>
-            {
-                return new PullRequest(
-                   x.ToString(),
-                   new Uri("https://github.com/wareismymind/doot/pulls/123"),
-                   //new Uri("https://github.com/titleThingasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd/doot/pulls/123"),
-                   new Descriptor("titleThingasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd", "Descriptybitasdasdaasdjfhgbaskdjhfgasdkjfhgaskdjfhgaskdjfhgasdkfs"),
-                   new State((PullRequestStatus)x, 10, 10),
-                   new GitInfo("refs/heads/doot", "refs/heads/main"));
-            }).ToList();
-
-            var compactFormatter = new CompactFormatter(new DefaultEmojiProvider());
-            var formatted = compactFormatter.FormatLines(prList).ToList();
-            consoleJiggy.Display(formatted, true, default);
+            Console.WriteLine(res);
         }
     }
 }
---
+++ Peer/Verbs/ShowOptions.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace Peer.Verbs
+{
+    [Verb("show", isDefault: true, HelpText = "Display pull requests assigned to your account")]
+    public class ShowOptions
+    {
+        //CN: No filters or anything atm. 
+    }
+}
