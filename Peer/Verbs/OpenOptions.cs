e86ac86f2031a2341f8109f35013e6ed3b9582fd
santhoshar-bt <srajan@beyondtrust.com>
2021-10-21 15:36:43 -0600

    fix: adding space between words

--- Peer/Verbs/OpenOptions.cs
+++ Peer/Verbs/OpenOptions.cs
@@ -9,7 +9,7 @@ namespace Peer.Verbs
             HelpText = "A full or partial pull request identifier of the form provider/org/repo/id" +
             " partials can be built up from combinations of sections. For example: '6' to get" +
             " pull the pull request with id 6 provided there is only one. If there are more than one match" +
-            "you'll have to disambiguate by adding additional qualifiers eg. repoName/6")]
+            " you'll have to disambiguate by adding additional qualifiers eg. repoName/6")]
         public string? Partial { get; set; }
     }
 }
