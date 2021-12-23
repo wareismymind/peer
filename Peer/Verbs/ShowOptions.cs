6db6571f317b24d4ea99e8bbdc56fa9132695c7e
Chris Nantau <Chris.Nantau@Gmail.com>
2021-12-23 14:01:45 -0400

    replace extra : in sort direction description with |

--- Peer/Verbs/ShowOptions.cs
+++ Peer/Verbs/ShowOptions.cs
@@ -6,7 +6,7 @@ namespace Peer.Verbs
     public class ShowOptions
     {
         [Option(shortName: 's', longName: "sort", Required = false, HelpText = "The sort key and direction you would like to use. " +
-            "The default sort direction is ascending. <key>:[(asc|ascending)|(desc:descending)]")]
+            "The default sort direction is ascending. <key>:[(asc|ascending)|(desc|descending)]")]
         public string? Sort { get; set; }
 
         [Option(shortName: 'w', longName: "watch", Required = false)]
